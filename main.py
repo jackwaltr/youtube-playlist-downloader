2fa8d116de567a268bcbc10b65c3245df25ef646
Jack <jackwaltr@gmail.com>
2019-06-09 19:21:58 -0400

    Create main.py
    main

---
+++ main.py
@@ -0,0 +1,9 @@
+from ytlinks import *
+from selchrome import *
+
+#put your link to youtube playlist down below
+def main():
+    downloadYoutube(getLinks("https://www.youtube.com/YOUR-LINK-HERE"))
+
+if __name__ == '__main__':
+    main()
